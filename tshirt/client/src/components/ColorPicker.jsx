afe866082e027d02b4f8b4c9aa25f3174f965a3b
meetlimbani27 <meetlimbani23@gmail.com>
2024-05-11 23:26:37 +0530

    made a new repo for ThreeJS and transfered project here

---
+++ tshirt/client/src/components/ColorPicker.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../store'
+
+const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
+
+  return (
+    <div className='absolute left-full ml-3'>
+      <SketchPicker
+      color = {snap.color}
+      disableAlpha
+      presetColors={['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF', '#800080', '#808000', '#008080', '#808080', '#FF69B4', '#DA70D6', '#FFC0CB', '#FFD166', '#00CED1', '#20B2AA', '#48D1CC', '#00FA9A', '#F08080', '#FA8072']}
+
+      onChange={(color) => state.color = color.hex}
+      />
+    </div>
+  )
+}
+
+export default ColorPicker
---
+++ tshirt/server/routes/dalle.routes.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import OpenAI from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+// const config = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(config);
+
+const openai = new OpenAI({
+    apiKey: "sk-proj-kDV9FxyVCbF47S6614RFT3BlbkFJtw555Xx233auixKvPAfY",
+});
+
+
+router.route('/').get((req, res) => {
+    res.status(200).json({ message: 'Hello from DALL-E! Routes'});
+})
+
+router.route('/').post(async(req, res) => {
+    try {
+        const { prompt} = req.body;
+
+        const response = await openai.images.generate({
+            prompt,
+            model: "dall-e-2",
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        });
+
+        const image = response.data.data[0].b64_json;
+        console.log("Generated image:",image);
+
+        res.status(200).json({ photo: image });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Something went wrong' });  
+    }
+});
+
+export default router;
